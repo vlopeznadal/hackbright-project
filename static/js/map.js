2d3af46fc0bc36504cc0e26e5faab026d1ef523f
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-09 10:43:24 -0600

    Map functionality

--- static/js/map.js
+++ static/js/map.js
@@ -1,13 +1,28 @@
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 44.9632328, lng: -93.2493657},
-        zoom: 3,
-        mapId: 'bf545e16b59f18e2'
-    });
+    $.get('/coordinates', response => {
+            const coordinates = response;
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: coordinates[1]["latitude"], lng: coordinates[1]["longitude"]},
+                zoom: 12,
+                mapId: 'bf545e16b59f18e2'
+            });
 
-    new google.maps.Marker({
-        position: myLatLng,
-        map,
-        title: "Marker",
+            for (const marker in coordinates) {
+                const mapMarker = new google.maps.Marker({
+                    position: {lat: coordinates[marker]["latitude"], lng: coordinates[marker]["longitude"]},
+                    map,
+                    title: "Cafe" + marker,
+                });
+                $.get('/markers', response => {
+                    const markerInfo = response;
+                    const infowindow = new google.maps.InfoWindow ({
+                        content: "<a href='cafes/"+ markerInfo[marker][1]+"'>" + markerInfo[marker][0] + "</a>",
+                    });
+
+                    mapMarker.addListener("click", () => {
+                        infowindow.open(map, mapMarker);
+                    });
+                });
+            }
     });
 }
