241df616f797f3358d12c03312f205c9493a97f7
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-09 14:12:13 -0600

    Centering map in marker bounds

--- static/js/map.js
+++ static/js/map.js
@@ -2,12 +2,14 @@ function initMap() {
     $.get('/coordinates', response => {
             const coordinates = response;
             map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: coordinates[1]["latitude"], lng: coordinates[1]["longitude"]},
-                zoom: 13,
+                center: new google.maps.LatLng(0, 0),
+				zoom: 0,
                 mapId: 'bf545e16b59f18e2'
             });
 
+            const LatLngList = [];
             for (const marker in coordinates) {
+                LatLngList.push(new google.maps.LatLng (coordinates[marker]["latitude"],coordinates[marker]["longitude"]));
                 const mapMarker = new google.maps.Marker({
                     position: {lat: coordinates[marker]["latitude"], lng: coordinates[marker]["longitude"]},
                     map,
@@ -20,13 +22,21 @@ function initMap() {
                 $.get('/markers', response => {
                     const markerInfo = response;
                     const infowindow = new google.maps.InfoWindow ({
-                        content: "<a href='cafes/"+ markerInfo[marker][1]+"'>" + markerInfo[marker][0] + "</a>",
+                        content: "<p><a href='cafes/"+ markerInfo[marker][1]+"'>" + markerInfo[marker][0] + "</a></p><p>" + markerInfo[marker][2] + "<br>" 
+                        + markerInfo[marker][3] + ", " + markerInfo[marker][4] + " " + markerInfo[marker][5] + "</p>",
                     });
 
                     mapMarker.addListener("click", () => {
                         infowindow.open(map, mapMarker);
                     });
                 });
+
+                const bounds = new google.maps.LatLngBounds ();
+                for (let i = 0, LtLgLen = LatLngList.length; i < LtLgLen; i++) {
+                    bounds.extend (LatLngList[i]);
+                }
+                map.fitBounds (bounds);
             }
+            console.log(LatLngList);
     });
 }
