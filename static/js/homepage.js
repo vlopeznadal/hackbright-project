34f3c201bc36affadb461bcc95b8e1ab5bd45a32
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-17 16:28:24 -0600

    JS to show and hide login/register forms

---
+++ static/js/homepage.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(document).on('click', '#create-account', function() { 
+            $('#login').replaceWith('<div name="register" id="register" class="d-flex flex-column min-vh-100 justify-content-center align-items-center form"><form action="/register" method="POST"><h1>Create an account</h1><div class="mb-3"><label for="register-email" class="form-label">Email address</label><input type="email" class="form-control" id="register-email" aria-describedby="emailHelp" name="register-email"></div><div class="mb-3"><label for="register-password" class="form-label">Password</label><input type="password" class="form-control" id="register-password" name="register-password"></div><button type="submit" class="btn btn-primary">Submit</button></form>Already have an account? <a href="#" id="sign-in">Sign in</a></div>');
+      });
+      
+      $(document).on('click', '#sign-in', function() { 
+          $('#register').replaceWith('<div name="login" id="login" class="d-flex flex-column min-vh-100 justify-content-center align-items-center form"><h1>Login</h1><form action="/login" method="POST"><div class="mb-3"><label for="login-email" class="form-label">Email address</label><input type="email" class="form-control" id="login-email" aria-describedby="emailHelp" name="login-email"></div><div class="mb-3"><label for="login-password" class="form-label">Password</label><input type="password" class="form-control" id="login-password" name="login-password"></div><button type="submit" class="btn btn-primary">Submit</button></form><a href="#" id="create-account">Create an account</a></div>');
+      });
+});
