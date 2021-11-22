603e58c73d08029306905ff6999e4b45308bcf80
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-21 20:16:19 -0600

    JS to display stars for cafe ratings

---
+++ static/js/ratings.js
@@ -0,0 +1,10 @@
+let starWidth = 16;
+
+$.fn.stars = function() {
+  return $(this).each(function() {
+    $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * starWidth));
+  });
+}
+$(document).ready(function() {
+  $('span.stars').stars();
+});
