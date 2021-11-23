d5afa54160402a10e77fdf405bb1842ebe84b4d4
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-23 13:02:19 -0600

    Comments added

--- static/js/background.js
+++ static/js/background.js
@@ -1,4 +1,9 @@
-$(window).on("load", function () {
+// Once HTML document has been loaded
+$(document).ready(function(){
+    // Setting variable to randomly selected background img URL
     const ranBackground = "/static/img/backgrounds/background-" + Math.floor((Math.random() * 2) + 1) + ".jpg";
+
+    // Setting the CSS property 'background-image' of DIV with class 'background' to 
+    // the randomly selected background 
     $(".background").css("background-image", "url("+ranBackground + ")");
 });
