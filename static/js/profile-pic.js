fdfd733f4c0e83f86701ea2af0a55e95496a55ba
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-23 20:41:34 -0600

    Added comments

--- static/js/profile-pic.js
+++ static/js/profile-pic.js
@@ -1,10 +1,23 @@
-$(window).on("load", function () {
-    $.post('/profile-pic', response => {
-        let profile_pic = response
-        if (profile_pic != "False") {
-          $('#profile-pic-display').append('<img src="' + profile_pic + '" class="reviewer_photo"/>');
-        } else {
-          $('#profile-pic-display').append('<img src="/static/img/default_reviewer.jpg" class="reviewer_photo"/>');
-        }
-    });
+// Once HTML document has been loaded
+$(document).ready(function(){
+
+  // Grabs the user's profile picture
+  $.post('/get-profile-pic', response => {
+
+    // Setting response to a variable
+    let profile_pic = response
+
+    // If the user's profile picture exists
+    if (profile_pic != "False") {
+      
+      // Display user's profile picture on page
+      $('#profile-pic-display').append('<img src="' + profile_pic + '" class="reviewer_photo"/>');
+    
+    // If user's profile picture doesn't exist
+    } else {
+
+      // Display default profile picture on page
+      $('#profile-pic-display').append('<img src="/static/img/default_reviewer.jpg" class="reviewer_photo"/>');
+    }
   });
+});
