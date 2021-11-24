b40f18f4a3490693ad47a455011b97f567fb4365
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-23 18:55:23 -0600

    Comments added

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,9 +1,19 @@
+// Listen for changes to the range input with id 'rating'
 $("#rating").on('change', function() {
+
+    // Creating a variable for the current value of the range input
     let current_value = $('input[type=range]').val();
-    $('.bubble').html(" " + current_value);
+
+    // Display range's current value in span with class 'range-value' on details.html
+    $('.range-value').html(" " + current_value);
   });
 
+  // Listen for changes to the range input with id 'updatedrating'
   $("#updatedrating").on('change', function () {
+
+    // Creating a variable for the current value of the range input
     let current_value = $('input[type=range]').val();
-    $('.bubble').html(" " + current_value);
+
+    // Display range's current value in span with class 'range-value' on details.html
+    $('.range-value').html(" " + current_value);
   });
