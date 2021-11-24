4d39defc27a399d68502950951c46f144526420b
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-23 21:04:14 -0600

    Comments added

--- static/js/ratings.js
+++ static/js/ratings.js
@@ -1,10 +1,20 @@
+// Creating variable to set star width
 let starWidth = 16;
 
+// Creating stars method
 $.fn.stars = function() {
+
+  // For each span with class 'stars' 
   return $(this).each(function() {
+
+    // Determine width of stars based on number in span
     $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * starWidth));
   });
 }
+
+// Once HTML document has been loaded
 $(document).ready(function() {
+
+  // Apply the stars method to the spans with class 'stars'
   $('span.stars').stars();
 });
