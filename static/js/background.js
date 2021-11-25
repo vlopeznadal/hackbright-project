d80034aab9d4920c89c6e8490f318d17af3ca4e9
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-11-24 20:04:39 -0600

    Edited to include increased background count

--- static/js/background.js
+++ static/js/background.js
@@ -1,7 +1,7 @@
 // Once HTML document has been loaded
 $(document).ready(function(){
     // Setting variable to randomly selected background img URL
-    const ranBackground = "/static/img/backgrounds/background-" + Math.floor((Math.random() * 2) + 1) + ".jpg";
+    const ranBackground = "/static/img/backgrounds/background-" + Math.floor((Math.random() * 5) + 1) + ".jpg";
 
     // Setting the CSS property 'background-image' of DIV with class 'background' to 
     // the randomly selected background 
