5ea823134942b2dd24ad861d9914fad26107a154
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-12-09 10:34:02 -0600

    Added reset listener

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,19 +1,29 @@
+// Listen for reset of the form with id 'updating'
+$("#updating").on('reset', function() {
+
+  // Creating a variable for the default value of 3
+  let current_value = 3;
+
+  // Display default value in span with class 'range-value' on details.html
+  $('.edit-range-value').html(" " + current_value);
+});
+
 // Listen for changes to the range input with id 'rating'
 $("#rating").on('change', function() {
 
-    // Creating a variable for the current value of the range input
-    let current_value = $('input[type=range]').val();
+  // Creating a variable for the current value of the range input
+  let current_value = $('input[type=range]').val();
 
-    // Display range's current value in span with class 'range-value' on details.html
-    $('.range-value').html(" " + current_value);
-  });
+  // Display range's current value in span with class 'range-value' on details.html
+  $('.review-range-value').html(" " + current_value);
+});
 
-  // Listen for changes to the range input with id 'updatedrating'
-  $("#updatedrating").on('change', function () {
+// Listen for changes to the range input with id 'updatedrating'
+$("#updatedrating").on('change', function () {
 
-    // Creating a variable for the current value of the range input
-    let current_value = $('input[type=range]').val();
+  // Creating a variable for the current value of the range input
+  let current_value = $('input[type=range]').val();
 
-    // Display range's current value in span with class 'range-value' on details.html
-    $('.range-value').html(" " + current_value);
-  });
+  // Display range's current value in span with class 'range-value' on details.html
+  $('.edit-range-value').html(" " + current_value);
+});
